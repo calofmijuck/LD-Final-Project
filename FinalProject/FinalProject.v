6f537a6f845efb390268efe5de9b8ba04c87777c
calofmijuck <calofmijuck@snu.ac.kr>
2018-06-13 18:06:15 +0900

    Test

---
+++ FinalProject/FinalProject.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:49:28 06/13/2018 
+// Design Name: 
+// Module Name:    FinalProject 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module FinalProject(
+    input [7:0] instruction,
+	 input [7:0] pc
+    input reset;
+	 );
+	 
+	 reg [31:0] data[3:0];
+	 
+	 
+
+
+endmodule
