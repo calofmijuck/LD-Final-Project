25ca992b7752562fbe0aeeb5e5a7b0cc0d98d107
KRKimDongWoo <dhsodhao52@naver.com>
2018-06-14 14:42:04 +0900

    Add files via upload

--- FinalProject/FinalProject.v
+++ FinalProject/FinalProject.v
@@ -10,10 +10,14 @@ module FinalProject(
 	 input showReg1,
 	 input showReg2,
 	 input showReg3,
+	 input [4:0] showData,
+	 output reg DCLED,
 	 output [6:0] seg1, 
-	 output [6:0] seg2, //hex segments
+	 output [6:0] seg2,
 	 output [6:0] seg3, 
-	 output [6:0] seg4, //hex segments
+	 output [6:0] seg4, 
+	 output [6:0] seg5,
+	 output [6:0] seg6, //hex segments
 	 output [7:0] readingAddress, //instruction address output
 	 output runningLED
 	 );
@@ -25,15 +29,20 @@ module FinalProject(
 	 reg [7:0] data[31:0]; //data memory
 	 reg [7:0] registers[3:0]; //4 registers
 	 reg [5:0] counting;
-	 reg [7:0] display = 8'b00000000;
+	 reg [7:0] display1 = 8'b00000000;
+	 reg [7:0] display2 = 8'b00000000;
+	 reg [7:0] readData;
 	 wire [4:0] showButton;
 	 assign showButton = {showReg0, showReg1, showReg2, showReg3, showPC};
 	 FrequencyDivider freqdiv (.CLKin(CLK), .clr(reset), .CLKout(contCLK));
+	 
 	 HextoBCD HBCD1 (.bcd(regWrite[7:4]), .segout(seg1));
 	 HextoBCD HBCD2 (.bcd(regWrite[3:0]), .segout(seg2));
-	 HextoBCD HBCD3 (.bcd(display[7:4]), .segout(seg3));
-	 HextoBCD HBCD4 (.bcd(display[3:0]), .segout(seg4));
-	  
+	 HextoBCD HBCD3 (.bcd(display1[7:4]), .segout(seg3));
+	 HextoBCD HBCD4 (.bcd(display1[3:0]), .segout(seg4));
+	 HextoBCD HBCD5 (.bcd(display2[7:4]), .segout(seg5));
+	 HextoBCD HBCD6 (.bcd(display2[3:0]), .segout(seg6));
+	 
 	 initial begin
 		pc = 8'b00000000;
 		for(counting = 0; counting < 32; counting = counting + 1) begin
@@ -43,14 +52,17 @@ module FinalProject(
 	 
 	 assign runningLED = contCLK;
 	 
+	 always @(showData) begin
+		display2 = data[showData];
+	 end
+	 
 	 always @(showButton) begin
 		case(showButton)
-			5'b10000 : display = registers[0];
-			5'b01000 : display = registers[1];
-			5'b00100 : display = registers[2];
-			5'b00010 : display = registers[3];
-			5'b00001 : display = pc;
-			default : display = 8'b11111111;
+			5'b10000 : display1 = registers[0];
+			5'b01000 : display1 = registers[1];
+			5'b00100 : display1 = registers[2];
+			5'b00010 : display1 = registers[3];
+			default : display1 = pc;
 		endcase
 	 end
 	 
@@ -71,15 +83,18 @@ module FinalProject(
 					registers[instruction[1:0]] = registers[instruction[3:2]] + registers[instruction[5:4]];
 					regWrite = registers[instruction[1:0]];
 					pc = pc + 1;
+					DCLED = 1'b1;
 				end
 				2'b01 : begin //load
 					registers[instruction[3:2]] = data[registers[instruction[5:4]] + instruction[1:0]];
 					regWrite = registers[instruction[3:2]];
 					pc = pc + 1;
+					DCLED = 1'b1;
 				end
 				2'b10 : begin //store
 					data[registers[instruction[5:4]] + instruction[1:0]] = registers[instruction[3:2]];
 					pc = pc + 1;
+					DCLED = 1'b0;
 				end
 				2'b11 : begin //jump
 					case(instruction[1:0])
@@ -88,6 +103,7 @@ module FinalProject(
 						2'b10 : pc = pc - 1;
 						2'b11 : pc = pc;
 					endcase
+					DCLED = 1'b0;
 				end
 			endcase
 		end
