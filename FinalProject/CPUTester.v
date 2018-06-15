a48ac01b66060cdae5f1a521bd48abbcb5c909fe
Sungchan Yi <38686321+calofmijuck@users.noreply.github.com>
2018-06-15 16:50:03 +0900

    Update CPUTester.v

--- FinalProject/CPUTester.v
+++ FinalProject/CPUTester.v
@@ -27,22 +27,18 @@ module CPUTester;
 	assign MemByte[1] = 8'b01001001;
 	assign MemByte[2] = 8'b00011001;
 	assign MemByte[3] = 8'b10000100;
-	
 	assign MemByte[4] = 8'b01000100;
 	assign MemByte[5] = 8'b01001001;
 	assign MemByte[6] = 8'b00011001;
 	assign MemByte[7] = 8'b10000100;
-	
 	assign MemByte[8] = 8'b01000100;
 	assign MemByte[9] = 8'b01001001;
 	assign MemByte[10] = 8'b00011001;
 	assign MemByte[11] = 8'b10000100;
-	
 	assign MemByte[12] = 8'b01000100;
 	assign MemByte[13] = 8'b01001001;
 	assign MemByte[14] = 8'b00011001;
 	assign MemByte[15] = 8'b10000100;
-	
 	assign MemByte[16] = 8'b01000100;
 	assign MemByte[17] = 8'b01001001;
 	assign MemByte[18] = 8'b00011001;
